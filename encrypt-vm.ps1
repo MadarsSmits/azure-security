e212c5ec8bed2ee49f1a39e3ca2f200ff8e322a8
Madars Šmits <Madars.Smits@outlook.lv>
2019-02-10 13:56:03 +0200

    Update 2

--- encrypt-vm.ps1
+++ encrypt-vm.ps1
@@ -9,4 +9,4 @@ $KeyVaultResourceId = $KeyVault.ResourceId;
 Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $KeyVaultRGName -EnabledForDiskEncryption
 Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $KeyVaultRGName -EnabledForDeployment
 Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $KeyVaultRGName -EnabledForTemplateDeployment
-Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $vmRGName -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $vmRGName -VMName $vmName -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId -SkipVmBackup
